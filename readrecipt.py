0c8a18cd41364eeb2a8f30d60fb76393a023e37e
James Shapas <j@jmba.local>
2022-08-19 21:40:17 -0400

    fixed scanner?

--- readrecipt.py
+++ readrecipt.py
@@ -1,16 +1,25 @@
 from PIL import Image
 import pytesseract
 
-ingredients = pytesseract.image_to_string(Image.open('testing2.jpeg')) # string of entire picture
+ingredients = pytesseract.image_to_string(Image.open('sample.jpg')) # string of entire picture
 
 new = ingredients.split("\n")
 
 items = []
+total = 0
 
 for item in new:
-    if "$" in item and "/" not in item:
+    if "total" in item.lower():
+        break
+
+    if "." in item and "/" not in item:
         items.append(item)
-    
+        price = float(item.split(" ")[-1])
+        total += price
+
+print("Items:")
 for item in items:
-    print(item)   
-    
+    print(item)
+
+print(f"Total: {total}")
+print(f"Total w/ Ontario Tax: {total*1.13}")
