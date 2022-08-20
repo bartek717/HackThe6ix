00b58cd4d063c9d2a58a224cebcf93b85a6f79cd
bartekkowalski465 <bartekkowalski925@gmail.com>
2022-08-20 07:32:16 -0400

    new layout

--- website/views.py
+++ website/views.py
@@ -8,3 +8,19 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html", user=current_user)
 
+@views.route('/upload')
+@login_required
+def upload():
+    return render_template("upload.html", user=current_user)
+
+@views.route('/overview')
+@login_required
+def overview():
+    return render_template("overview.html", user=current_user)
+
+@views.route('/view')
+@login_required
+def view():
+    return render_template("view.html", user=current_user)
+    
+
