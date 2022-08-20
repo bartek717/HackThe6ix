1191ff022a37761d5f5bc50bbb2eb0d0d995e9ec
bartek717 <bartekkowalski925@gmail.com>
2022-08-20 00:15:08 -0400

    sleep

--- website/views.py
+++ website/views.py
@@ -1,14 +1,10 @@
-from flask import Blueprint, render_template, request, Response
-from werkzeug.utils import secure_filename
+from flask import Blueprint, render_template
 from flask_login import  login_required, current_user
-from db import db_init, db
-from models import Img
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['POST', 'GET'])
+@views.route('/')
 @login_required
 def home():
-    
     return render_template("home.html", user=current_user)
 
