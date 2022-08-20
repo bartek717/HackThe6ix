bcf1e65dc00c10ff739cec5d4ba1e6ad735cd330
bartek717 <bartekkowalski925@gmail.com>
2022-08-20 00:04:04 -0400

    goodnight

--- website/models.py
+++ website/models.py
@@ -26,3 +26,9 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+
+class Img(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    img = db.Column(db.Text, unique=True, nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    mimetype = db.Column(db.Text, nullable=False)
--- website/views.py
+++ website/views.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, Response
+from werkzeug.utils import secure_filename
 from flask_login import  login_required, current_user
+from db import db_init, db
+from models import Img
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['POST', 'GET'])
 @login_required
 def home():
+    
     return render_template("home.html", user=current_user)
 
