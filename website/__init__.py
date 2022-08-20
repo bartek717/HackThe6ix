d5d6b1b34db38e4a92f269857af845ce2fcb0220
bartek717 <bartekkowalski925@gmail.com>
2022-08-19 22:13:27 -0400

    database setup?

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = 'databse.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Jeffrey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,31 @@
-from flask import Blueprint
+from curses import flash
+import curses
+from unicodedata import category
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p> login <p>"
+    data = request.form
+    print(data)
+    print()
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p> logout <p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p> sign up <p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+
+        
+        
+    return render_template("sign_up.html")
 
